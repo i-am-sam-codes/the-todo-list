6e28ef36f7cc2f50e94e1a7a53e2ec05436f4e5b
Sam Ceja <sammmceja@gmail.com>
2023-05-22 16:48:56 -0700

    creates nav module

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
-import _ from "lodash";
+import createNav from "./nav";
 
 function component() {
+  const contentDiv = document.getElementById("content");
   const element = document.createElement("div");
 
-  // Lodash, currently included via a script, is required for this line to work
-  element.innerHTML = _.join(["Hello", "webpack"], " ");
-
-  return element;
+  element.appendChild(createNav());
+  contentDiv.appendChild(element);
 }
 
 document.body.appendChild(component());
---
+++ src/nav.js
@@ -0,0 +1,16 @@
+export default function createNav() {
+  const contentDiv = document.getElementById("content");
+
+  const navContainer = document.createElement("div");
+  navContainer.classList.add("nav");
+
+  const navBar = document.createElement("div");
+  navBar.textContent = "my todo list";
+  navBar.style.fontSize = "30px";
+  navBar.style.backgroundColor = "blue";
+  navBar.style.padding = "20px";
+
+  navContainer.appendChild(navBar);
+
+  contentDiv.appendChild(navContainer);
+}
